1b260bb44952fadcd546c8acd43cc6e1e78abc17
Khaled Nadam <khalednadam@gmail.com>
2024-08-31 02:02:54 +0300

    feat: :sparkles: add form validation on login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,27 +1,48 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify-icon/react";
 import { signIn } from "next-auth/react";
 import { CardContent, CardFooter } from "@/components/ui/card";
 import Link from "next/link";
-import httpStatus from "http-status";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useToast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+
+const FormSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(4, "Password must have at least 4 characters"),
+});
 
 const LoginPage = () => {
   const { toast } = useToast();
-  const email = useRef("");
-  const password = useRef("");
   const router = useRouter();
   const [isPasswordVisible, setIsPasswordVisible] = useState(false);
-
-  const onSubmit = async () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
     const res = await signIn("credentials", {
-      email: email.current,
-      password: password.current,
+      email: values.email,
+      password: values.password,
       redirect: false,
       callbackUrl: "/",
     });
@@ -39,42 +60,55 @@ const LoginPage = () => {
   return (
     <>
       <CardContent>
-        <form onSubmit={onSubmit}>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="john@example.com"
-                onChange={(e) => (email.current = e.target.value)}
-              />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="password">Password</Label>
-              <div className="relative w-full max-w-sm">
-                <Input
-                  id="password"
-                  type={isPasswordVisible ? "text" : "password"}
-                  placeholder="your password"
-                  onChange={(e) => (password.current = e.target.value)}
-                ></Input>
-                <Button
-                  type="button"
-                  variant="ghost"
-                  size="icon"
-                  className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
-                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                >
-                  <Icon
-                    icon={isPasswordVisible ? "ph:eye-slash" : "ph:eye"}
-                    width={24}
-                  ></Icon>
-                </Button>
-              </div>
-            </div>
-          </div>
-        </form>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="john@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <div className="relative w-full max-w-sm">
+                      <Input
+                        type={isPasswordVisible ? "text" : "password"}
+                        placeholder="your password"
+                        {...field}
+                        className="pr-10"
+                      />
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="icon"
+                        className="absolute right-1 top-1/2 -translate-y-1/2 h-7 w-7 text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-100"
+                        onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                      >
+                        <Icon
+                          icon={isPasswordVisible ? "ph:eye-slash" : "ph:eye"}
+                          width={24}
+                        ></Icon>
+                      </Button>
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </CardContent>
 
       <CardFooter className="flex flex-col w-full">
@@ -82,7 +116,7 @@ const LoginPage = () => {
           <Button variant="link">Don't have an account? Create one</Button>
         </Link>
 
-        <Button className="w-full" onClick={onSubmit}>
+        <Button className="w-full" onClick={form.handleSubmit(onSubmit)}>
           Login
         </Button>
       </CardFooter>
