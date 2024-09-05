0d44274d96b39473df362bc1a77a34eb8bfdaf49
Khaled Nadam <khalednadam@gmail.com>
2024-09-06 00:34:29 +0300

    feat: :sparkles: logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div>
+      <p>
+        <span className="font-bold">f4b</span>.dev
+      </p>
+    </div>
+  );
+};
+
+export default Logo;
