5fd303e211968396af5572762a7463cfed105185
Khaled Nadam <khalednadam@gmail.com>
2024-09-08 22:14:27 +0300

    feat: :sparkles: make navbar item link

--- src/components/SideNavBar/NavBarItem.tsx
+++ src/components/SideNavBar/NavBarItem.tsx
@@ -1,37 +1,43 @@
 import React from "react";
 import { Button } from "../ui/button";
 import { AnimatePresence, motion } from "framer-motion";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface NavBarItemProps {
   isMini?: boolean;
   Icon: React.ReactNode;
   Text: string;
+  to: string;
 }
 
-const NavBarItem = ({ isMini = false, Icon, Text }: NavBarItemProps) => {
+const NavBarItem = ({ isMini = false, Icon, Text, to }: NavBarItemProps) => {
+  const pathName = usePathname();
   return (
-    <Button
-      variant="ghost"
-      className={`gap-2 justify-start`}
-      size={isMini ? "icon" : "default"}
-    >
-      <div className={`${isMini && "flex justify-center items-center mx-2"}`}>
-        {Icon}
-      </div>
+    <Link href={to}>
+      <Button
+        variant={pathName === to ? "secondary" : "ghost"}
+        className={`gap-2 justify-start w-full`}
+        size={isMini ? "icon" : "default"}
+      >
+        <div className={`${isMini && "flex justify-center items-center mx-2"}`}>
+          {Icon}
+        </div>
 
-      <AnimatePresence>
-        {!isMini && (
-          <motion.div
-            initial={{ opacity: 0, x: -20 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -20 }}
-            transition={{ duration: 0.1 }}
-          >
-            {Text}
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </Button>
+        <AnimatePresence>
+          {!isMini && (
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -20 }}
+              transition={{ duration: 0.1 }}
+            >
+              {Text}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Button>
+    </Link>
   );
 };
 
--- src/components/SideNavBar/SideNavBarMenuItems.tsx
+++ src/components/SideNavBar/SideNavBarMenuItems.tsx
@@ -1,6 +1,8 @@
-import { Home, TrendingUp, Search, Bookmark, User } from "lucide-react";
+import { Home, TrendingUp, Search, Bookmark, User, Plus } from "lucide-react";
 import React from "react";
 import NavBarItem from "./NavBarItem";
+import { Button } from "../ui/button";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface SideNavBarMenuItemsProps {
   isMini?: boolean;
@@ -9,11 +11,52 @@ interface SideNavBarMenuItemsProps {
 const SideNavBarMenuItems = ({ isMini = false }: SideNavBarMenuItemsProps) => {
   return (
     <>
-      <NavBarItem Icon={<Home />} Text="Home" isMini={isMini} />
-      <NavBarItem Icon={<TrendingUp />} Text="Trending" isMini={isMini} />
-      <NavBarItem Icon={<Search />} Text="Search" isMini={isMini} />
-      <NavBarItem Icon={<Bookmark />} Text="Saved" isMini={isMini} />
-      <NavBarItem Icon={<User />} Text="Profile" isMini={isMini} />
+      <NavBarItem Icon={<Home />} Text="Home" isMini={isMini} to="/" />
+      <NavBarItem
+        Icon={<TrendingUp />}
+        Text="Trending"
+        isMini={isMini}
+        to="/trending"
+      />
+      <NavBarItem
+        Icon={<Search />}
+        Text="Search"
+        isMini={isMini}
+        to="/search"
+      />
+      <NavBarItem
+        Icon={<Bookmark />}
+        Text="Saved"
+        isMini={isMini}
+        to="/saved"
+      />
+      <NavBarItem
+        Icon={<User />}
+        Text="Profile"
+        isMini={isMini}
+        to="/profile"
+      />
+      <Button
+        className={`gap-2 justify-start w-full items-center bg-foreground`}
+        size={isMini ? "icon" : "default"}
+      >
+        <div className={`${isMini && "flex justify-center items-center mx-2"}`}>
+          <Plus />
+        </div>
+
+        <AnimatePresence>
+          {!isMini && (
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -20 }}
+              transition={{ duration: 0.1 }}
+            >
+              Add Project
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Button>
     </>
   );
 };
