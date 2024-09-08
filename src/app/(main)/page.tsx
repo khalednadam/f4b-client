6fb55baa9e4d6136d6154e3751f535f040b948e0
Khaled Nadam <khalednadam@gmail.com>
2024-09-08 22:14:44 +0300

    feat: :sparkles: setup main page and settings page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,17 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Search } from "lucide-react";
+
 export default function Home() {
   return (
-    <div className="flex flex-col items-start justify-between p-24">main</div>
+    <div className="flex flex-col items-start justify-between p-10">
+      <div className="w-full justify-center flex flex-col items-center">
+        <h1 className="md:text-6xl text-4xl font-bold">Search for projects</h1>
+        <div className="md:w-2/3 w-full py-10 relative">
+          <Input placeholder="Search for projects"></Input>
+          <Search className="absolute right-3 top-1/2 -translate-y-1/2" />
+        </div>
+      </div>
+    </div>
   );
 }
